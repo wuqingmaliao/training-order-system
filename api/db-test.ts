0af5a5676125aab6e3493ca069b9f174f4cb3cc4
wuqingmaliao <2923935255@qq.com>
2026-08-12 20:47:07 +0800

    db-test改用require加载postgres

--- api/db-test.ts
+++ api/db-test.ts
@@ -1,4 +1,4 @@
-import postgres from 'postgres';
+const postgres = require('postgres');
 
 export default async function handler(req: any, res: any) {
   try {
@@ -14,14 +14,13 @@ export default async function handler(req: any, res: any) {
       connect_timeout: 10,
     });
 
-    const result = await sql`SELECT current_database() as db, current_user as user, version() as version`;
+    const result = await sql`SELECT current_database() as db, current_user as usr`;
     await sql.end();
 
     res.status(200).json({
       success: true,
       db: result[0].db,
-      user: result[0].user,
-      version: result[0].version,
+      user: result[0].usr,
     });
   } catch (err: any) {
     res.status(500).json({
